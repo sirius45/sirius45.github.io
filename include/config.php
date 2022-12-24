9a8c200ce331f9492715347becf315376345fef4
sirius45 <121359916+sirius45@users.noreply.github.com>
2022-12-25 03:21:34 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<astro','mikhmon>|>aWNlbGZob2k=');
+
+$data['siundulwifi'] = array ('1'=>'siundulwifi!10.20.30.1','siundulwifi@|@astro','siundulwifi#|#aWNlbGZob2k=','siundulwifi%si undul wifi','siundulwifi^aniun.net','siundulwifi&Rp','siundulwifi*10','siundulwifi(1','siundulwifi)','siundulwifi=10','siundulwifi@!@disable');
